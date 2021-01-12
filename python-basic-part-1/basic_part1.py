ed6e6a1cbc843d6315417400bdb9714a042185e2
alibaltaci <alibaltaci13@gmail.com>
2021-01-13 01:56:47 +0300

    question 4 solved

--- python-basic-part-1/basic_part1.py
+++ python-basic-part-1/basic_part1.py
@@ -47,3 +47,17 @@ import datetime
 now = datetime.datetime.now()
 print("Current date time:")
 print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+
+# 4. Write a Python program which accepts the radius of a circle from the user and compute the area.
+
+"""
+Sample Output :
+r = 1.1
+Area = 3.8013271108436504
+"""
+
+from math import pi
+r = float(input("Input the radius of the circle:"))
+print("The area of the circle with radius {} is {}".format(str(r), str(pi * r**2)))
