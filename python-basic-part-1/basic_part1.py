8931b018e51c18cf0bdfc0e9ced7561b9e6790cc
alibaltaci <alibaltaci13@gmail.com>
2021-01-12 21:02:01 +0300

    remote eklendi

--- python-basic-part-1/basic_part1.py
+++ python-basic-part-1/basic_part1.py
@@ -21,3 +21,4 @@ Twinkle, twinkle, little star,
 
 print("Twinkle, twinkle, little star, \n\t\tHow I wonder what you are!\n\t\t\t\tHow I wonder what you are!"
       "\n\t\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are")
+
