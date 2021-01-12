5adf22b521c01b7eed4930ae95f1d884608aec30
alibaltaci <alibaltaci13@gmail.com>
2021-01-12 20:33:15 +0300

    question 1 solved

---
+++ python-basic-part-1/basic_part1.py
@@ -0,0 +1,23 @@
+# Python Basic (Part -I) - Exercises
+# https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+
+
+# 1. Write a Python program to print the following string in a specific format (see the output).
+
+"""
+Sample String : "Twinkle, twinkle, little star,
+                How I wonder what you are! Up above the world so high,
+                Like a diamond in the sky. Twinkle, twinkle,
+                little star, How I wonder what you are" Output :
+
+Twinkle, twinkle, little star,
+	    How I wonder what you are!
+		        Up above the world so high,
+		        Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	    How I wonder what you are
+"""
+
+print("Twinkle, twinkle, little star, \n\t\tHow I wonder what you are!\n\t\t\t\tHow I wonder what you are!"
+      "\n\t\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are")
