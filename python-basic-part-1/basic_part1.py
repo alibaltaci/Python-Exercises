642cd4257d28ee42af31f03d06b66f2aed0107c3
alibaltaci <alibaltaci13@gmail.com>
2021-01-14 13:24:31 +0300

    question 10 solved

--- python-basic-part-1/basic_part1.py
+++ python-basic-part-1/basic_part1.py
@@ -135,3 +135,19 @@ Sample Output : The examination will start from : 11 / 12 / 2014
 
 exam_st_date = (1, 1, 1995)
 print("The axamination will start from: %i / %i / %i" % exam_st_date)
+
+
+
+
+# 10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn
+
+"""
+Sample value of n is 5
+Expected Result : 615
+"""
+
+n = int(input("Input a number: "))
+n1 = int("%s" % n)
+n2 = int("%s%s" % (n,n))
+n3 = int("%s%s%s" % (n,n,n))
+print("Result for the number {}: {}".format(n, (n1+n2+n3)))
