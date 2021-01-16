0b4f4a148575dbc127a7f3a6a2ad77f1c47b7864
alibaltaci <alibaltaci13@gmail.com>
2021-01-16 13:18:33 +0300

    question 13 solved

--- python-basic-part-1/basic_part1.py
+++ python-basic-part-1/basic_part1.py
@@ -181,3 +181,22 @@ def calen(y, m):
       print(calendar.month(y, m))
 
 calen(1995, 1)
+
+
+
+# 13. Write a Python program to print the following here document.
+
+"""
+Sample string:
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+"""
+
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
