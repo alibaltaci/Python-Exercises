8bdce4edc611f2664ba55f23a80fac4684ce66bb
alibaltaci <alibaltaci13@gmail.com>
2021-01-16 13:29:34 +0300

    question 14 solved

--- python-basic-part-1/basic_part1.py
+++ python-basic-part-1/basic_part1.py
@@ -200,3 +200,19 @@ This
 is a  ....... multi-line
 heredoc string --------> example
 """)
+
+
+
+# 14. Write a Python program to calculate number of days between two dates.
+
+"""
+Sample dates : (2014, 7, 2), (2014, 7, 11)
+Expected output : 9 days
+"""
+
+from datetime import date
+first_date = date(1995, 1, 1)
+last_date = date(2021, 1, 16)
+delta = last_date - first_date
+print(delta.days)
+
